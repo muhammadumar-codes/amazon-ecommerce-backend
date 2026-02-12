bb3a55c0fd5af5c555e664e68e7d3a5a51d8190c
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-12 13:25:25 +0500

    update register and login schema using yup

--- validations/auth.validation.js
+++ validations/auth.validation.js
@@ -34,7 +34,6 @@ export const registerSchema = yup.object({
 })
 
 // ================================* LOGIN SCHEMA *============================
-import * as yup from 'yup'
 
 export const loginSchema = yup.object({
   email: yup
