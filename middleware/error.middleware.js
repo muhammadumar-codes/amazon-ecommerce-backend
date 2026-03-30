296225ca90935fa8de290a58f116d4c769170034
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-03-30 10:05:59 +0500

    upate the error handling

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,10 +1,36 @@
 /* ================================* GLOBAL ERROR HANDLER *=============================== */
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let message = err.message
 
+  /* =====*** MONGODB INVALID OBJECT ID ***===== */
+  if (err.name === 'CastError') {
+    statusCode = 400
+    message = 'Invalid ID format'
+  }
+
+  /* =====*** MONGODB DUPLICATE KEY ERROR ***===== */
+  if (err.code === 11000) {
+    statusCode = 400
+    message = 'Duplicate field value entered'
+  }
+
+  /* =====*** JWT ERROR ***===== */
+  if (err.name === 'JsonWebTokenError') {
+    statusCode = 401
+    message = 'Invalid token'
+  }
+
+  /* =====*** JWT EXPIRED ***===== */
+  if (err.name === 'TokenExpiredError') {
+    statusCode = 401
+    message = 'Token expired'
+  }
+
+  /* =====*** RESPONSE ***===== */
   res.status(statusCode).json({
     success: false,
-    message: err.message,
+    message,
     stack: process.env.NODE_ENV === 'development' ? err.stack : null,
   })
 }
